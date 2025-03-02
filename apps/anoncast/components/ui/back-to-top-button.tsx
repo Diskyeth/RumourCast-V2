4ccf24b777c0465a4feab88cd541408ee6380109
Diskyeth <evert@evertmartin.be>
2025-03-02 19:18:50 +0100

    small layout improvements

--- apps/anoncast/components/ui/back-to-top-button.tsx
+++ apps/anoncast/components/ui/back-to-top-button.tsx
@@ -12,7 +12,11 @@ const BackToTopButton = () => {
 <button
   onClick={scrollToTop}
   className="fixed bottom-6 right-6 z-10 w-14 h-14 flex items-center justify-center bg-black/70 text-white rounded-full shadow-xl transition-transform duration-300 hover:scale-105 gradient-border-wrapper"
-  style={{ background: "rgba(0, 0, 0, 0.7) !important" }}
+  style={{
+    background: "rgba(0, 0, 0, 0.7) !important",
+    backdropFilter: "blur(8px)", // Adjust blur intensity as needed
+    WebkitBackdropFilter: "blur(8px)", // Ensures support in Safari
+  }}
   aria-label="Back to Top"
 >
       <svg
--- apps/anoncast/components/ui/hamburger-menu-button.tsx
+++ apps/anoncast/components/ui/hamburger-menu-button.tsx
@@ -25,13 +25,19 @@ export default function HamburgerMenuButton() {
   return (
     <>
   
-      <button
-        className="flex right-6 z-50 w-14 h-14 flex items-center justify-center bg-black/70 text-white rounded-full shadow-xl transition-transform duration-300 hover:scale-10 gradient-border-wrapper hover:scale-105"
-        onClick={toggleMenu}
-        aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
-      >
-        {isMenuOpen ? '✕' : '☰'}
-      </button>
+  <button
+  className="flex right-6 z-50 w-14 h-14 flex items-center justify-center bg-black/70 text-white rounded-full shadow-xl transition-transform duration-300 hover:scale-105 gradient-border-wrapper"
+  onClick={toggleMenu}
+  aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+  style={{
+    background: "rgba(0, 0, 0, 0.7) !important",
+    backdropFilter: "blur(8px)", // Adjust blur intensity as needed
+    WebkitBackdropFilter: "blur(8px)", // Safari support
+  }}
+>
+  {isMenuOpen ? '✕' : '☰'}
+</button>
+
 
    
       {isMenuOpen &&
@@ -48,9 +54,12 @@ export default function HamburgerMenuButton() {
           <a href="https://www.geckoterminal.com/base/pools/0xe43e9d214a4bcb01c2fade45359bea37e74f314e" target="_blank" rel="noopener noreferrer" className="font-bold hover:scale-105 hover:rotate-2 hover:text-primary">
             GeckoTerminal
           </a>
+          <a href="https://web3.bitget.com/en/swap/base/0x1CEcCbE4d3a19cB62DbBd09756A52Cfe5394Fab8?isShowHint=false" target="_blank" rel="noopener noreferrer" className="font-bold hover:scale-105 hover:rotate-2 hover:text-primary">
+            Bitget
+          </a>
         </div>
         <div className="flex flex-col items-center gap-6"> 
-          <a href="https://github.com/leovido/rumourcast" target="_blank" rel="noopener noreferrer" className="font-light hover:scale-105 hover:rotate-2 hover:text-primary">
+          <a href="https://github.com/Diskyeth/RumourCast-V2" target="_blank" rel="noopener noreferrer" className="font-light hover:scale-105 hover:rotate-2 hover:text-primary">
             Github
           </a>
           <a href="https://paragraph.xyz/@disky.eth/rumourcast-roadmap" target="_blank" rel="noopener noreferrer" className="font-light hover:scale-105 hover:rotate-2 hover:text-primary">
